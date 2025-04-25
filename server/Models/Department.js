e4c35325d7cec7004e5ae248cbd839397a837345
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-25 17:32:00 +0530

    Create Department.js

---
+++ server/Models/Department.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const DepartmentSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true 
+    },
+    code: {
+        type: String,
+        required: true,
+        unique: true,
+        uppercase: true, 
+        trim: true
+    },
+    faculty: {
+        type: String,
+        required: true, 
+    },
+    description: {
+        type: String
+    },
+    headOfDepartment: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        default: null
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    }
+}, { timestamps: true });
+
+const Department = mongoose.model('Department', DepartmentSchema);
+
+module.exports = Department;
