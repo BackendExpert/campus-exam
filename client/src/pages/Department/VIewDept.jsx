a52a47c5a47f2e275c4b9c764913bf7b2c95fbeb
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-27 07:44:58 +0530

    Update VIewDept.jsx

--- client/src/pages/Department/VIewDept.jsx
+++ client/src/pages/Department/VIewDept.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import secureLocalStorage from 'react-secure-storage'
 
@@ -8,10 +9,32 @@ const ViewDept = () => {
     const role = secureLocalStorage.getItem('loginR')
     const email = secureLocalStorage.getItem('loginE')
     const token = localStorage.getItem('login')
-    
-    return (
-        <div>ViewDept</div>
-    );
+
+    const [viewdept, setviewdept] = useState([])
+
+    useEffect(() => {
+        axios.get(import.meta.env.VITE_APP_API + '/department/getonedepartment/' + id, {
+            headers: { 'Authorization': `Bearer ${token}` },
+        })
+            .then(res => setviewdept(res.data.Result))
+            .catch(err => console.log(err))
+    }, [])
+
+
+    if (role === 'superadmin') {
+        return (
+            <div>
+                <h1 className="">Department ID: {id}</h1>
+            </div>
+        )
+    }
+    else {
+        useEffect(() => {
+            localStorage.clear()
+            window.location.reload()
+        }, [])
+    }
+
 };
 
 export default ViewDept;
