1fe73d6e29f0db11074037dcde9a9ed01b26598f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-04-27 10:25:37 +0530

    Update subjectRoute.js

--- server/Routes/subjectRoute.js
+++ server/Routes/subjectRoute.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const { authMiddleware } = require('../Middlewares/AuthMiddleware');
+const { accessMiddleware } = require('../Middlewares/AccessMiddleware');
+const subjectController = require('../Controllers/subjectController');
 
 const router = express.Router();
+
+router.post('/createsubject', authMiddleware, accessMiddleware(['superadmin', 'examadmin']), subjectController.createSubject)
 
 module.exports = router;
